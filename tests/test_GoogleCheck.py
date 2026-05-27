6200b3dfaac7d51d015fab83d58655986ec40a77
JennHenderson-arch <sharon.boyd@sdbei.com>
2026-05-26 20:28:47 -0400

    changes to the two tests that should still work but also invoke tracing. JEH

--- tests/test_GoogleCheck.py
+++ tests/test_GoogleCheck.py
@@ -1,11 +1,11 @@
-#20260625 - JEH updated to pull CI ststus and run headless. Else for debug local runs it will run with headed browser and pause for 5 seconds to allow for visual confirmation of the test running. This is a simple test that checks if the title of the Google homepage is "Google". It uses Playwright's expect API to assert the title and includes a debug pause that only activates when not running in a CI environment.
+#20260625 - JEH updated to pull CI ststus and run headless. Else for debug local runs it will run with headed browser and pause for 5 seconds to allow for visual confirmation of the test running. This is a simple test that checks if the title of the Google homepage is "Google". It uses Playwright's expect API to assert the title and includes a debug pause that only activates when not running in a CI environment. "is_ci" is on conftest.py  
 
 import time
 from playwright.sync_api import expect
 
 
-
-def test_google_title(browser_type, is_ci):
+## going to try something new.   
+""" def test_google_title(browser_type, is_ci):
     browser = browser_type.launch(headless=is_ci)
     page = browser.new_page()
     
@@ -17,6 +17,12 @@ def test_google_title(browser_type, is_ci):
     expect(page).to_have_title("Google")
     print("Google title is correct.")
 
-    browser.close()
+    browser.close() """
 
-    
+
+def test_google_title(page, is_ci):
+    page.goto("https://www.google.com")
+    time.sleep(5)
+    expect(page).to_have_title("Google")
+    print("Google title is correct.")
+    page.close()
--- tests/test_playwright01.py
+++ tests/test_playwright01.py
@@ -1,9 +1,11 @@
+#20260625 - JEH updated to pull CI ststus and run headless. Else for debug local runs it will run with headed browser and pause for 5 seconds to allow for visual confirmation of the test running. This is a simple test that checks if the title of the Google homepage is "Google". It uses Playwright's expect API to assert the title and includes a debug pause that only activates when not running in a CI environment. "is_ci" is on conftest.py  
+
 import time
 
 from playwright.sync_api import expect
 
 
-def test_example_title(browser_type, is_ci):
+""" def test_example_title(browser_type, is_ci):
     browser = browser_type.launch(headless=is_ci)
     page = browser.new_page()
     page.goto("https://example.com")
@@ -12,5 +14,12 @@ def test_example_title(browser_type, is_ci):
 
     expect(page).to_have_title("Example Domain")
     print("Title is correct.")
-    browser.close()
+    browser.close() """
+
+def test_example_title(page, is_ci):
+    page.goto("https://example.com")
+    time.sleep(5)
+    expect(page).to_have_title("Example Domain")
+    print("Title is correct.")
+    page.close()
  
